b9901c1f0b6d2c572f5a261937972c7c83970dec
Michael Iskandarani <mike.iskandarani@gmail.com>
2020-05-23 17:14:25 -0500

    Implemented endpoint for resizing images

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import { tmpdir } from "os";
+import multer from "multer";
+import { join, extname } from "path";
+import { Storage } from "@google-cloud/storage";
+import sharp from "sharp";
+import fs from "fs-extra";
+
+const gcs = new Storage();
+const bucket = gcs.bucket(process.env.BUCKET);
+
+const workingDir = join(tmpdir(), "images");
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+const upload = multer({ dest: workingDir });
+
+app.post("/images", upload.array("images", 12), async (req, res) => {
+  const sizes = [64, 128, 256, 512];
+
+  const uploadImages = (req.files as Express.Multer.File[])
+    .map((file) => {
+      // Create directory to dump resized images
+      const resizeDir = join(workingDir, "resize", file.filename);
+      fs.ensureDirSync(resizeDir);
+
+      // Return array of bucket upload promises
+      return sizes.map(async (size) => {
+        const resizedName = `${size}w${extname(file.originalname)}`;
+        const resizedPath = join(resizeDir, resizedName);
+
+        // Resize images to temporary file
+        await sharp(file.path)
+          .resize({
+            width: size,
+          })
+          .toFile(resizedPath);
+
+        // Map to bucket upload promise
+        return bucket.upload(resizedPath, {
+          destination: `images/${file.filename}/${resizedName}`,
+          gzip: true,
+        });
+      });
+    })
+    .reduce((prev, curr) => {
+      // Flatten array
+      return prev.concat(curr);
+    });
+
+  // Upload images to bucket
+  await Promise.all(uploadImages);
+  await fs.remove(workingDir);
+  res.send("Images Uploaded!");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
