f1d7c4a2345fc9d0429d6fa9bd6da3e1ea07da88
Michael Iskandarani <mike.iskandarani@gmail.com>
2020-06-30 20:09:45 -0500

    Made decrypt secret executable

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,7 +2,5 @@
 
 # Decrypt the file
 mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSWORD" \
 --output $HOME/secrets/admin-secret.json admin-secret.json.gpg
