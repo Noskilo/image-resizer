cb5af9c5a8f54a9b0c481314dbd4d9dbc24c2288
Michael Iskandarani <mike.iskandarani@gmail.com>
2020-06-30 21:05:35 -0500

    Changed secret path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSWORD" \
---output $HOME/secrets/admin-secret.json .github/secrets/admin-secret.json.gpg
+--output .github/secrets/admin-secret.json .github/secrets/admin-secret.json.gpg
