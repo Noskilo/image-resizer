7ef200aa1b9542f8962fcb11efadd6870ec7c53f
Michael Iskandarani <mike.iskandarani@gmail.com>
2020-07-01 12:00:27 -0500

    Added test for gifs

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -3,42 +3,49 @@ import { app, bucket } from "../src/app";
 import { join, basename } from "path";
 
 test("bucket is defined", () => {
-  expect(process.env.BUCKET).toBeDefined();
-  expect(bucket).toBeDefined();
+    expect(process.env.BUCKET).toBeDefined();
+    expect(bucket).toBeDefined();
 });
+
 
 describe("resizing images", () => {
-  test("POST request to /images resizes all images", async (done) => {
+    test("POST request to /images resizes all images", async (done) => {
+
+        await resizeRequest("/assets/space.jpg");
+        await resizeRequest("/assets/space-boy.gif");
+        done();
+    });
+});
+
+async function resizeRequest(path: string) {
     const { status, body } = await request(app)
-      .post("/resize")
-      .field("sizes", [64, 128, 256])
-      .attach("images", join(__dirname, "/assets/space.jpg")).catch(() => null);
+        .post("/resize")
+        .field("sizes", [63, 128, 256])
+        .attach("images", join(__dirname, path)).catch(() => null);
 
     expect(status).toBe(200);
-    expect(body.sizes.length).toBeGreaterThan(0);
+    expect(body.sizes.length).toBe(3);
 
     for (const image of body.images) {
-      const prefix = image.location;
+        const prefix = image.location;
 
-      const [files] = await bucket.getFiles({
-        autoPaginate: false,
-        delimiter: "/",
-        prefix,
-      }).catch(() => []);
+        const [files] = await bucket.getFiles({
+            autoPaginate: false,
+            delimiter: "/",
+            prefix,
+        }).catch(() => []);
 
-      expect(files).toBeDefined();
-      const imageSizes = files.map((file) =>
-        Number.parseInt(basename(file.name).replace("w", ""))
-      );
+        expect(files).toBeDefined();
+        const imageSizes = files.map((file) =>
+            Number.parseInt(basename(file.name).replace("w", ""))
+        );
 
-      expect(imageSizes.sort()).toEqual(body.sizes.sort());
+        expect(imageSizes.sort()).toEqual(body.sizes.sort());
 
-      await bucket.deleteFiles({
-        delimiter: "/",
-        prefix,
-      }).catch(() => null);
+        await bucket.deleteFiles({
+            delimiter: "/",
+            prefix,
+        }).catch(() => null);
     }
+}
 
-    done();
-  });
-});
