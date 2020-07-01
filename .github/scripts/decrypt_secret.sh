0f94058aadd796e9781dbf4b666aab9965f938cd
Michael Iskandarani <mike.iskandarani@gmail.com>
2020-06-30 20:52:02 -0500

    Moved gpg to secrets

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSWORD" \
---output $HOME/secrets/admin-secret.json ./admin-secret.json.gpg
+--output $HOME/secrets/admin-secret.json $HOME/secrets/admin-secret.json.gpg
